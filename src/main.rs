9e62468f484f58fca55b5683fb56ea8dac0e2bdb
YoungPhilGim <youngphil.gim@gmail.com>
2023-08-16 13:26:28 +0900

    fixed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("{}", query);
+    println!("{}", file_path);
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Shoud have been able to read the file");
+
+    print!("{}", contents);
 }
