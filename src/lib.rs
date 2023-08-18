5b440fa5972385aa170c4e979d21f10718fd99d0
YoungPhilGim <youngphil.gim@gmail.com>
2023-08-18 10:59:06 +0900

    fixed

--- src/lib.rs
+++ src/lib.rs
@@ -9,19 +9,19 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build<A>(mut args: A) -> Result<Config, &'static str>
-    where A: Iterator<Item = String>
-    {
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
         args.next();
 
         let query = match args.next() {
             Some(arg) => arg,
             None => return Err("Didn't get a query string"),
         };
+
         let file_path = match args.next() {
             Some(arg) => arg,
             None => return Err("Didn't get a file path"),
         };
+
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
         Ok(Config {
